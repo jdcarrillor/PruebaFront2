c5a6e0c2cb31b67a81b0e6c4caffccd22cbaf730
pruebavalienta <daniel.brawn.carrillo@gmail.com>
2020-01-09 21:45:44 -0500

    aaa

---
+++ build/precache-manifest.b4b639b7ffc41825e711db138c99f11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "202344f205070cce469f",
+    "url": "/static/css/main.15ba623b.chunk.css"
+  },
+  {
+    "revision": "202344f205070cce469f",
+    "url": "/static/js/main.593d2623.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c997e7fd689d3c1e6f86",
+    "url": "/static/css/2.804d63bb.chunk.css"
+  },
+  {
+    "revision": "c997e7fd689d3c1e6f86",
+    "url": "/static/js/2.01d2e0b4.chunk.js"
+  },
+  {
+    "revision": "b131aebb51629b7b85ce2f4a6a02d7e4",
+    "url": "/index.html"
+  }
+];
